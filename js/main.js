97c02e29226bc3a5738f33de6ab3162e79cd82d9
Leyla Abbasova <Leyla Abbasova>
2016-08-10 14:38:12 +0400

    scroll top

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// $(document).ready(function(){
+//     $(".col-md-1:nth-child(1)").hover(function(){
+//         $(".col-md-1:nth-child(2)").css("background-color", "yellow");
+//         $(".col-md-1:nth-child(3)").css("background-color", "white");
+//     })
+//     $(".col-md-1:nth-child(2)").hover(function(){
+//     	$(".col-md-1:nth-child(3)").css("background-color", "green");
+//          $(".col-md-1:nth-child(4)").css("background-color", "white");
+//     })
+//     $(".col-md-1:nth-child(3)").hover(function(){
+//     	$(".col-md-1:nth-child(4)").css("background-color", "blue");
+//         $(".col-md-1:nth-child(5)").css("background-color", "white");
+    
+//     })
+// });
+
+
